80aa77c545fa761d6bdc957a123f7afc52576130
Tori Machen <tmachen@pillartechnology.com>
2019-02-25 11:37:19 -0600

    Able to run SpringBoot

--- src/main/java/com/revolvingbunnies/Foo.java
+++
@@ -1,7 +0,0 @@
-package com.revolvingbunnies;
-
-public class Foo {
-  public static void main(String[] args) {
-    System.out.print("Running!");
-  }
-}
--- src/test/java/com/revolvingbunnies/ApplicationTest.java
+++ src/test/java/com/revolvingbunnies/ApplicationTest.java
@@ -3,7 +3,7 @@ package com.revolvingbunnies;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class FooTest {
+public class ApplicationTest {
 
     @Test
     public void thisShouldPass() {
