81ed27ab88b57ffa71eec9732646bba8b26aed22
Tori Machen <tmachen@pillartechnology.com>
2019-02-25 11:42:00 -0600

    Add first RestController

---
+++ src/main/java/com/revolvingbunnies/HelloController.java
@@ -0,0 +1,13 @@
+package com.revolvingbunnies;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "Greetings from my bunny Clarence!";
+  }
+}
