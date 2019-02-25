7759e62e5e377c24a9ac72c4226d275cb4df640a
Tori Machen <tmachen@pillartechnology.com>
2019-02-25 10:56:58 -0600

    added junit 5 as a dependency

---
+++ src/test/java/com/revolvingbunnies/FooTest.java
@@ -0,0 +1,12 @@
+package com.revolvingbunnies;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+
+    @Test
+    public void thisShouldPass() {
+        Assertions.assertTrue(true);
+    }
+}
